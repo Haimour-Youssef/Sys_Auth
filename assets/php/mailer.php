9e06798d6f9694efdb648c2f3ef5a3e8009c0c88
Haimour-Youssef <80226825+Haimour-Youssef@users.noreply.github.com>
2024-01-14 22:07:10 +0100

    Update mailer.php

--- assets/php/mailer.php
+++ assets/php/mailer.php
@@ -23,8 +23,8 @@ function sendmeil($ClientMail, $code, $name = null, $phone = null)
         $mail->isSMTP();                                          //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                 //Enable SMTP authentication
-        $mail->Username   = 'origamy.dev@gmail.com';              //SMTP username
-        $mail->Password   = 'hrmylecyvdmhxrhr';                   //SMTP password
+        $mail->Username   = 'example@gmail.com';              //SMTP username
+        $mail->Password   = 'pop';                   //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          //Enable implicit TLS encryption
         $mail->Port       = 465;                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         $mail->SMTPOptions = array(
